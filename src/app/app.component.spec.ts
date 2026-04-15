a7600bf0f7174dd1ed7542129c4a73f5493b9279
Alejandro Tomba <82008062+aletomba@users.noreply.github.com>
2026-04-15 10:15:45 -0300

    fix: resolve 30 vulnerabilities via npm audit fix (#9)
    Closes #8

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,15 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Portfolio'`, () => {
+  it('should have themeService', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Portfolio');
+    expect(app.themeService).toBeTruthy();
   });
 
-  it('should render title', () => {
+  it('should toggle dark mode', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Portfolio app is running!');
+    const app = fixture.componentInstance;
+    app.toggleDarkMode();
   });
 });
